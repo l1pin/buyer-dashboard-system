a366505a8ddec670173529d147b274dab6a3a007
l1pin <123176883+l1pin@users.noreply.github.com>
2025-10-01 08:57:51 +0300

    Update UserManagement.js

--- src/components/UserManagement.js
+++ src/components/UserManagement.js
@@ -439,7 +439,7 @@ function UserManagement({ user }) {
   }
 
   return (
-    <div className="h-full flex flex-col bg-gray-50">
+    <div className="bg-gray-50">
       {/* Header */}
       <div className="bg-white border-b border-gray-200 px-6 py-4">
         <div className="flex items-center justify-between">
