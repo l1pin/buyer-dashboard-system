496fe1d85f7dac78c519b4c88e965e9e75c98f53
l1pin <123176883+l1pin@users.noreply.github.com>
2025-10-06 15:59:55 +0300

    Update BuyerCreativePanel.js

--- src/components/BuyerCreativePanel.js
+++ src/components/BuyerCreativePanel.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { creativeService, creativeHistoryService } from '../supabaseClient';
+import { creativeService, creativeHistoryService, supabase } from '../supabaseClient';
 import { useBatchMetrics } from '../hooks/useMetrics';
 import { useZoneData } from '../hooks/useZoneData';
 import {
